42daf1e411bbe77349bc1f645ebb317dc69a4e70
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-06-13 22:14:48 -0400

    new auto scaler

--- lspplot/angular.py
+++ lspplot/angular.py
@@ -10,6 +10,36 @@ from pys import test
 from cmaps import pastel_clear,plasma_clear,viridis_clear,magma_clear_r;
 import re;
 
+def totalKE(d, ecut=0, anglecut=None,return_bools=False):
+    '''
+    Get total energy across a pext plane.
+
+    Parameters and Keywords
+    -----------------------
+    d            -- array
+    ecut         -- energy cut in eV
+    anglecut     -- None or a tuple of (angle, dimension) where dimension
+                    is "2D" or "3D". If None or falsey, don't cut on angle.
+    return_bools -- return the boolean array that selects uncut particles.
+
+    Returns total energy.
+    '''
+    good = d['KE'] > ecut;
+    if anglecut:
+        angle, dim = anglecut;
+        if dim == '2D':
+            good &= np.abs(d['phi']) > np.pi - angle/180*np.pi;
+        elif dim == '3D':
+            good &= np.cos(angle/180*np.pi) < -np.sin(d['theta'])*np.cos(d['phi']);
+        else:
+            raise ValueError("anglecut is not None, '2D' or '3D'");
+    KE = (np.abs(d['q'][good]*1e-6)*d['KE'][good]).sum();
+    if return_bools:
+        return KE,good;
+    else:
+        return KE;
+
+
 def angular_load(
         fname,F=None,normalize=False,polar=False,
         keV=False,abs_charges=True):
@@ -39,22 +69,15 @@ def angular_load(
         s=np.abs(s);
     if F is not None: s*=F;
     if normalize:
-         kw = normalize
-         Efactor = kw['max_e']/kw['energy_bins'];
-         if keV: Efactor *= 1e-3;
-         s /= Efactor*2*np.pi/kw['angle_bins'];
+        kw = normalize
+        if kw['max_e'] == 'auto' or kw['max_e'] == 'round':
+            mxe = np.max(e);
+        else:
+            mxe = kw['max_e'];
+        Efactor = mxe/kw['energy_bins'];
+        if keV: Efactor *= 1e-3;
+        s /= Efactor*2*np.pi/kw['angle_bins'];
     return s,phi,e,d
-
-def _str2cmap(i):    
-    if i == 'viridis':
-        return viridis_clear;
-    elif i == 'plasma':
-        return plasma_clear;
-    elif i == 'magma_r':
-        return magma_clear_r;
-    elif i == 'pastel':
-        return pastel_clear;
-    pass;
 
 defaults = {
     'tlabels': ['Forward\n0$^{\circ}$',
@@ -90,8 +113,10 @@ defaults = {
 def getkw(kw,label):
     return kw[label] if test(kw,label) else defaults[label];
 def getkw_kev(kw,label,kev):
+    if test(kw,label):
+        return kw[label];
     if kev: label+='_kev';
-    return getkw(kw,label);
+    return defaults[label];
 def angular(s, phi, e,
             colorbar=True,**kw):
     '''
@@ -103,7 +128,8 @@ def angular(s, phi, e,
       e   -- energies of each charge.
 
     Keyword Arugments:
-      max_e       -- Maximum energy.
+      max_e       -- Maximum energy, if 'auto',
+                     bin automatically.
       max_q       -- Maximum charge.
       angle_bins  -- Set the number of angle bins.
       energy_bins -- Set the number of energy bins.
@@ -113,7 +139,7 @@ def angular(s, phi, e,
       labels      -- Set the angular labels. If not a list, if
                      'default', use default. If 'tdefault', use
                      default for theta. (See defaults dict);
-      keV         -- Use keV isntead of MeV.
+      keV         -- Use keV instead of MeV.
       fig         -- If set, use this figure, Otherwise,
                      make a new figure.
       ax          -- If set, use this axis. Otherwise,
@@ -124,17 +150,41 @@ def angular(s, phi, e,
       cmap        -- use the colormap cmap.
       rgridopts   -- pass a dictionary that sets details for the
                      rgrid labels.
+      oap         -- Plot this half angle if not None.
+      efficiency  -- notify the conversion efficiency in a backwards
+                     angle.
     '''
     kev = test(kw, 'keV');
     phi_spacing = getkw(kw,'angle_bins');
     E_spacing =   getkw(kw,'energy_bins');
     maxE  = getkw_kev(kw,'max_e',kev);
+    Estep = getkw_kev(kw,'e_step',kev);
+    if maxE == 'max':
+        maxE = np.max(e);
+    elif maxE == 'round' or maxE == 'auto':
+        mxe = np.max(e);
+        tenpow = np.floor(np.log10(mxe))
+        maxE = 10**tenpow * (int(mxe/(10**tenpow))+1)
+        Estep = maxE/4.0;
+        if 4.0 > maxE > 2.0:
+            Estep = 1.0;
+        elif 2.0>= maxE > 1.0:
+            Estep = 0.5:
+        elif Estep < 1.0:
+            Estep = int(Estep/0.25)*0.25;
+        else:
+            #round up Estep to either a multiple of five or two
+            nearest2 = int(Estep / 2.0) * 2;
+            nearest5 = int(Estep / 5.0) * 5;
+            if abs(nearest2 - Estep) < abs(nearest5 - Estep):
+                Estep = nearest2;
+            else:
+                Estep = nearest5;
     maxQ  = getkw(kw,'max_q');
     minQ  = getkw(kw,'min_q');
-    Estep = getkw_kev(kw,'e_step',kev);
     clabel = getkw(kw,'clabel');
     cmap = getkw(kw, 'cmap');
-
+    
     phi_bins = np.linspace(-np.pi,np.pi,phi_spacing+1);
     E_bins   = np.linspace(0, maxE, E_spacing+1);
             
@@ -213,6 +263,8 @@ def angular(s, phi, e,
         mkline(ths, np.ones(ths.shape)*minr)
         mkline(mint*np.ones(ths.shape), rs);
         mkline(maxt*np.ones(ths.shape), rs);
+        if test(kw, 'efficiency'):
+            ;
     if test(kw,'labels'):
         if kw['labels'] == 'default':
             labels = defaults['labels'];
@@ -252,8 +304,6 @@ def _prep(opts):
     kw = {
         'angle_bins' : float(opts['--angle-bins']),
         'energy_bins': float(opts['--energy-bins']),
-        'max_e': float(opts['--max-e']) if opts['--max-e'] else (
-            getdef_kev('max_e')),
         'max_q': float(opts['--max-q']) if opts['--max-q'] else None,
         'min_q': float(opts['--min-q']) if opts['--min-q'] else None,
         'keV': kev,
@@ -266,6 +316,13 @@ def _prep(opts):
         'oap': float(opts['--oap']) if opts['--oap'] != 'none' else None,
         'log_q': opts['--log10'],
     };
+    if opts['--max-e']:
+        try:
+            kw['max_e']=float(opts['--max-e']);
+        except ValueError:
+            kw['max_e']=opts['--max-e'];
+    else:
+        kw['max_e'] = getdef_kev('max_e');
     cmap = _str2cmap(opts['--cmap']);
     if not cmap:
         cmap = opts['--cmap'];

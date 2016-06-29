febee78c12b54326645268185aa816bee6aa40f5
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-06-28 23:44:27 -0400

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lspplot',
-      version='0.0.1',
+      version='0.0.2',
       description='lsp plots',
       url='http://github.com/noobermin/lspplot',
       author='noobermin',

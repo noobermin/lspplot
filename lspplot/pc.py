363053fb554aa4d8e7fa3564360373a603730753
Gregory Ngirmang <ngirmang.1@osu.edu>
2016-10-20 19:20:32 -0400

    removing lims?

--- lspplot/pc.py
+++ lspplot/pc.py
@@ -98,11 +98,9 @@ def pc(q,p=None,**kw):
     if type(norm) is SymLogNorm:
         mnl = int(np.floor(np.log10(-mn)));
         mxl = int(np.floor(np.log10( mx)));
-        ticks=( [vmin]
-                  + [ -10.0**x for x in range(mnl,-linthresh-1,-1)]
-                  + [  0.0 ]
-                  + [  10.0**x for x in range(-linthresh,mxl+1)    ]
-                  + [vmax] )
+        ticks=( [  -10.0**x for x in range(mnl,-linthresh-1,-1)]
+                + [  0.0 ]
+                + [ 10.0**x for x in range(-linthresh,mxl+1)] )
         cbar = plt.colorbar(mypc,ticks=ticks);
     else:
         cbar = plt.colorbar(mypc);

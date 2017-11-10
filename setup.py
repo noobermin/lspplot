759f8c9523c3341875b7e807f54ee5e48e2236af
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-11-10 18:54:57 -0500

    adding revision

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lspplot',
-      version='0.0.12r0',
+      version='0.0.12r1',
       description='lsp plots',
       url='http://github.com/noobermin/lspplot',
       author='noobermin',

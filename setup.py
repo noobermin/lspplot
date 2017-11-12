de022cd2b1acee0de946d6604dfd1efed16df13d
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-11-12 18:28:53 -0500

    version bump temp

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lspplot',
-      version='0.0.12',
+      version='0.0.13r0',
       description='lsp plots',
       url='http://github.com/noobermin/lspplot',
       author='noobermin',

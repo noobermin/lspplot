44444162b3372fd056c35a83b2a3a12f5028516e
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2018-11-11 15:39:16 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='lspplot',
-      version='0.1.2r1',
+      version='0.1.3',
       description='lsp plots',
       url='http://github.com/noobermin/lspplot',
       author='noobermin',
       author_email='ngirmang.1@osu.com',
       license='MIT',
       install_requires=[
-          'pys>=0.0.12',
+          'pys>=0.0.12r1',
           'lspreader>=0.1.7',
           'matplotlib>=2.0.0',
           'numpy>=1.10.4',
